61b14f617a4ffa8f756a77d09840724714383dce
ayooubsmatti <ayooubsmatti@gmail.com>
2022-03-22 05:02:41 +0100

    Rendering Data to Templates

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,33 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import HttpResponse
 
+projectsList = [
+    {'id': '1',
+     'title': 'Portfolio',
+     'descriptions': 'PortfolioPortfolioPortfolioPortfolioPortfolioPortfolio',
+     },
+    {'id': '2',
+     'title': 'SOCIAL NETWORK ',
+     'descriptions': 'SOCIAL NETWORKSOCIAL NETWORKSOCIAL NETWORKSOCIAL NETWORKSOCIAL NETWORKSOCIAL NETWORK',
+     },
+    {'id': '3',
+     'title': 'Python',
+     'descriptions': 'PythonPythonPythonPythonPythonPython',
+     },
+    ]
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    number = 11
+    context = {'page':page,'number':number,'projects':projectsList}
+    return render(request, 'projects/projects.html',context)
     
 def project(request,pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html',{'project':projectObj})
