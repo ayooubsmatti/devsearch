b7e72ec2bc56d250e4ee45ba6269d3f479463a0f
ayooubsmatti <ayooubsmatti@gmail.com>
2022-03-22 04:08:17 +0100

    Templates & Template Inheritance

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('projects')
+    return render(request, 'projects/projects.html')
     
 def project(request,pk):
-    return HttpResponse('single project'+ ' ' + str(pk))
+    return render(request, 'projects/single-project.html')
