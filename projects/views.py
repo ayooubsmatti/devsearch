3c4b92eb55235fdb1b48c1fe2dc226882ea18593
ayooubsmatti <ayooubsmatti@gmail.com>
2022-03-22 03:38:42 +0100

    Views & URL's

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('projects')
+    
+def project(request,pk):
+    return HttpResponse('single project'+ ' ' + str(pk))
